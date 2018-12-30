57fd9ea292eaf6e676a78d042bc73fe1a8b8176f
x13311 <Mariano.Joison@wnco.com>
2018-12-30 14:01:14 -0300

    Mariano Joison - Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCLZOOsI4R89PnK5hiCeeRRtkfzavDKa6U",
+    authDomain: "historia-clinica.firebaseapp.com",
+    databaseURL: "https://historia-clinica.firebaseio.com",
+    projectId: "historia-clinica",
+    storageBucket: "historia-clinica.appspot.com",
+    messagingSenderId: "791060215440"
+  }
+};
